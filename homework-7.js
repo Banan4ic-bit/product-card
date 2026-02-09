afa51c0129540be93f6157b437ceac424b962823
Banan4ik <lord68771@gmail.com>
2026-02-09 19:31:06 +0200

    fix errors

--- homework-7.js
+++ homework-7.js
@@ -4,34 +4,27 @@ import { comments } from "./comments.js";
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const numbersForFive = numbers.filter(number => number >= 5)
-
 console.log(numbersForFive);
 
 //3. Массив фильмов и проверка наличия
 
-const movies = ['Человек паук','Железный человек', 'Халк', 'Стражи Галактики', 'Джокер']
+const movies = ['человек паук','железный человек', 'халк', 'стражи галактики', 'джокер']
 
-const input = prompt("Введите название фильма");
-const field = input[0].toUpperCase() + input.slice(1).toLowerCase();
-
-const movie = movies.includes(field);
+const movie = movies.includes(prompt("Введите название фильма").trim().toLowerCase());
 console.log(movie);
 
 //4. Перевернуть массив без изменения оригинала
 
 function reverseArray(array) {
-  console.log("Результат метода reverse:\n" + [...array].reverse());
+  return [...array].reverse();
 }
 
-reverseArray(numbers);
-reverseArray(movies);
-
-
+console.log ("Результат метода reverse:\n" + reverseArray(numbers));
+console.log ("Результат метода reverse:\n" + reverseArray(movies));
 
 // 7. Отфильтровать комментарии с ".com" в почте
 
-const emailsWithCom= comments.filter(comment => comment.email.includes(".com"))
-
+const emailsWithCom = comments.filter(comment => comment.email.includes(".com"))
 console.log(emailsWithCom);
 
 //8. Обновить postId в зависимости от id
@@ -63,7 +56,6 @@ console.log(commentsWithIsInvalid)
 
 //11. Получить массив почт через reduce и map
 
-
 const emailsFromReduce = comments.reduce((acc, comment) => {
   acc.push(comment.email);
   return acc
@@ -72,9 +64,7 @@ const emailsFromReduce = comments.reduce((acc, comment) => {
 console.log(emailsFromReduce);
 
 const emailsFromMap = comments.map(comment => comment.email)
-
 console.log(emailsFromMap)
-
 
 //12. Преобразовать массив почт в строку
 
