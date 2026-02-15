45781cc7b51de0b1b7f0fdf9be3bc9d35c50824f
Banan4ik <lord68771@gmail.com>
2026-02-15 22:11:00 +0200

    completed task with template

---
+++ homework-8.js
@@ -0,0 +1,74 @@
+import { productCards } from "./cards.js";
+
+const productCardTemplate = document.getElementById('product_card-template');
+const productCardList = document.getElementById('product_card_list');
+
+// 4: Преобразовать массив продуктов в объект с помощью .reduce(),
+// где ключи — названия продуктов, а значения — их описания.
+
+const productCardNamesWithDescription = productCards.reduce((acc, card) => {
+  acc.push({[card.name]: card.description})
+  return acc
+},[])
+
+console.log(productCardNamesWithDescription);
+
+//// 5: При загрузке страницы запросить у пользователя число от 1 до 5 через prompt.
+// Реализовать валидацию ввода и две функции: одна получает кол-во карточек, 
+// вторая — рендерит соответствующее число элементов на страницу
+
+function getCardsCount() {
+  const input = prompt("Сколько карточек отобразить? От 1 до 5");
+
+  if (input === null){
+    alert("Вы не ввели число, поэтому хуй тебе, а не карточки")
+    return null
+  }
+
+  const count = Number(input.trim());
+  
+  if (Number.isInteger(count) && count >=1 && count <=5){
+    return count
+  } else {
+    alert("Вы ввели направильное число")
+    return null
+  }
+}
+
+const count = getCardsCount()
+
+function setText(clone, selector, value) {
+  clone.querySelector(selector).textContent = value
+}
+
+function setSrc(clone, selector, value) {
+  clone.querySelector(selector).src =value
+}
+
+function renderCards(cardsArray) {
+  productCardList.innerHTML = ""
+
+  cardsArray.forEach(card => {
+    const productCardClone = productCardTemplate.content.cloneNode(true)
+
+    setSrc(productCardClone, '.product_card_photo', card.photo)
+
+    setText(productCardClone, '.product_card_category', card.category)
+    setText(productCardClone, '.product_card_name', card.name)
+    setText(productCardClone, '.product_card_description', card.description)
+    setText(productCardClone, '.product_card_price_value', `${card.price} ₽`)
+    
+    const compoundList = productCardClone.querySelector('.product_card_compound');
+    card.compound.forEach (item => {
+      const li = document.createElement('li');
+      li.textContent = item;
+      compoundList.appendChild(li)
+    })
+    productCardList.appendChild(productCardClone)
+  })
+}
+
+if (count !== null) {
+  const visibleCards = productCards.slice(0, count)
+  renderCards(visibleCards)
+}
